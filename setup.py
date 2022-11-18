399238f3fc49da85d941c725149f49a04e217aaa
Thenoahb <njbesst@gmail.com>
2022-11-18 13:34:55 -0500

    CIT483 Project 2 Part 2 setup.py and Makefile complete.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+with open("README.rst", encoding = "UTF-8") as f:
+	readme = f.read()
+
+setup(
+	name = "pgbackup",
+	version = "0.1.0",
+	description = "Creates a backup of my database locally or to AWS S3",
+	long_description = readme,
+	author = "Noah Bessler",
+	author_email = "besslern2@nku.edu",
+	packages = find_packages("src"),
+	package_dir = {"":"src"},
+	install_requires = []
+	)
