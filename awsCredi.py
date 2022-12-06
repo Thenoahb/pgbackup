d77f4c103b619d2b5def11a6177d2de4fe06642d
Thenoahb <118561001+Thenoahb@users.noreply.github.com>
2022-12-06 18:34:54 -0500

    This was not needed

--- awsCredi.py
+++
@@ -1,5 +0,0 @@
-
-import boto3
-def myAwsCred():
-dynamodb = boto3.resource('dynamodb',aws_access_key_id="User your access key", aws_secret_access_key="use your secret access key", region_name="us-east-2",endpoint_url="http://localhost:8000")
-return dynamodb
